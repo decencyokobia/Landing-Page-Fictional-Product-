24ec994f2e0b3a8c9b837d05183b4f8aa9588f30
Decency Okobia <learncodingdee@yahoo.com>
2025-04-22 14:46:01 +0100

    buy now and see feature buttons made functional to link to the right areas on the page

--- main.js
+++ main.js
@@ -9,19 +9,27 @@ function closeSidebar(){
 };
 
 
-document.getElementById("about_section").addEventListener("click", function() {
+document.getElementById("about_section").addEventListener("click", () => {
     window.location.href = "#about";
     sidebar.style.display = "none";
 });
 
 
-document.getElementById("feature_section").addEventListener("click", function() {
+document.getElementById("feature_section").addEventListener("click", () => {
     window.location.href = "#features-items";
     sidebar.style.display = "none";
 });
 
 
-document.getElementById("form_section").addEventListener("click", function() {
+document.getElementById("form_section").addEventListener("click", () => {
     window.location.href = "#form";
     sidebar.style.display = "none";
 });
+
+document.getElementById('buyNow').addEventListener('click', () => {
+    window.location.href = "#form";
+});
+
+document.getElementById('seeFeatures').addEventListener('click', () => {
+    window.location.href = "#features-items"
+})
